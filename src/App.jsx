a35be1209569c75dd7bbaf1145f531e4de307b47
blaze <blazejklimas006@gmail.com>
2026-08-26 17:54:37 +0200

    Added Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,47 @@
-import { PersonInfo } from "./components/PersonInfo"
+import { useState } from "react";
+import { Form } from "./components/Form";
+import { PersonInfo } from "./components/PersonInfo";
 
-const people=[
-  { 
-    name:"Tomek",
+const people = [
+  {
+    name: "Tomek",
     tel: 445890221,
-    city: "Warszawa"
+    city: "Warszawa",
   },
   {
-    name:"Karol",
+    name: "Karol",
     tel: 445833221,
-    city: "Częstochowa"
+    city: "Częstochowa",
   },
   {
-    name:"Bartek",
+    name: "Bartek",
     tel: 223216677,
-    
-  }
-
+  },
 ];
 
 function App() {
-  
+  const [isFormShown, setIsFormShown] = useState(false);
+  const handleShowFormClick = () => setIsFormShown(true);
+
   return (
     <>
       <h1>Lista kontaktów</h1>
+      {isFormShown ? (
+        <Form />
+      ) : (
+        <button onClick={handleShowFormClick}> Dodaj</button>
+      )}
+
       {people.map((person) => (
         <PersonInfo
-          key= {person.tel} 
-          name={person.name} 
-          tel={person.tel} 
-          city={person.city}/>
+          key={person.tel}
+          name={person.name}
+          tel={person.tel}
+          city={person.city}
+        />
       ))}
     </>
-  )
-};
+  );
+}
 
 export default App;
